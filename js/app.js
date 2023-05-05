9042218ffbf202db012d812bb983f90147068844
sandhya.chari <sandhyachari@sandhya.chari>
2023-05-05 20:45:52 +0530

    Final commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+AOS.init();
+
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbyB6IsB-1qCHtjNEPnasN7BSHadLxdnkojVh4QJ6SpR6_ZV1l5aYXhwsw_R3WS6EH7-kw/exec'
+// const form = document.forms['submit-to-google-sheet']
+// const msg= document.getElementById("msg")
+// form.addEventListener('submit', e => {
+//   e.preventDefault()
+//   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//     .then(response => {
+//       msg.innerHTML = "Your message sent sucessfully"
+//       setTimeout(function(){
+//         msg.innerHTML = ""
+//       },5000)
+//       form.reset()
+//     })
+//     .catch(error => console.error('Error!', error.message))
+// })
+
